fcfce9d477acbb9623386caee682f7c8d9faae7b
alokhosamani03-sys <alokhosamani03@gmail.com>
2026-03-20 15:11:16 +0530

    Add files via upload

---
+++ class factorial.java
@@ -0,0 +1,16 @@
+import java.util scanner;
+public class factorial
+{
+public static void main (string a[])
+{
+scanner sc:new scanner (System.in);
+System.out.println("enter the number");
+int num:sc.next int ();
+int factorial+1;
+for(int i=1: i<num;i++)
+{
+factorial factorial*i;
+}
+System, out.println(factorial of "+num+"is"+factorial);
+}
+}
